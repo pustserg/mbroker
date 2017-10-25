37375c0504a7579db4305b37d689e56ee704c5da
Sergey Pustovalov <pustserg@yandex.ru>
2017-10-25 23:43:22 +0300

    take free queue from pool

--- queues_pool/pool.go
+++ queues_pool/pool.go
@@ -5,7 +5,21 @@ type Pool struct {
 	queues []Queue
 }
 
-// AddQueue adds queue to pool
-func (pool *Pool) AddQueue(q Queue) {
+func (pool *Pool) addQueue(q Queue) {
 	pool.queues = append(pool.queues, q)
+}
+
+// Return free queue by name, or creates new queue with given name
+func (pool *Pool) GiveFreeQueue(name string) Queue {
+	var queue Queue
+	for i:=0; i < len(pool.queues); i++ {
+		if pool.queues[i].name == name {
+			queue = pool.queues[i]
+		}
+	}
+	if queue.name == "" {
+		queue = Queue{name: name, inputChannel: make(chan string), outputChannel: make(chan string)}
+		pool.addQueue(queue)
+	}
+	return queue
 }
